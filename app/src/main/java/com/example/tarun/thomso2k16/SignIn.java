7774be787da03e6c5e0f555dd074d9b85fabbbbc
tarun81296 <tarunbansal33405@gmail.com>
2016-08-09 11:53:10 +0530

    commit

--- app/src/main/java/com/example/tarun/thomso2k16/SignIn.java
+++ app/src/main/java/com/example/tarun/thomso2k16/SignIn.java
@@ -1,57 +1,86 @@
 package com.example.tarun.thomso2k16;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Html;
 import android.view.Gravity;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.params.CoreProtocolPNames;
+import org.apache.http.util.EntityUtils;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
 public class SignIn extends AppCompatActivity {
-public EditText userName;
+    public EditText userName;
     public EditText password;
     public String mUsername;
     public String mPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
-        password= (EditText) findViewById(R.id.password_edittext);
-        userName= (EditText) findViewById(R.id.username_edittext);
+        password = (EditText) findViewById(R.id.password_edittext);
+        userName = (EditText) findViewById(R.id.username_edittext);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case android.R.id.home:
-                Intent i = new Intent(SignIn.this,NavigationDrawerPage.class);
+                Intent i = new Intent(SignIn.this, NavigationDrawerPage.class);
                 startActivity(i);
                 finish();
                 return true;
         }
         return super.onOptionsItemSelected(item);
     }
-    public void submitClicked(View view){
-        mPassword=password.getText().toString();
-        mUsername=userName.getText().toString();
-       if(mUsername.length()==0){
-           erroredit(userName,"Username can't be empty");
-       }
-        else{
-           if(mPassword.length()==0){
-               erroredit(password,"Password can't be empty");
-           }
-           else{
 
-           }
-       }
+    public void submitClicked(View view) {
+        mPassword = password.getText().toString();
+        mUsername = userName.getText().toString();
+        if (mUsername.length() == 0) {
+            erroredit(userName, "Username can't be empty");
+        } else {
+            if (mPassword.length() == 0) {
+                erroredit(password, "Password can't be empty");
+            } else {
+                LoginAsyncTask async_Event_Post = new LoginAsyncTask();
+                async_Event_Post.execute("http://www.thomso.in/quiz/login.php");
+            }
+        }
     }
+
     private void erroredit(EditText editname, String msg) {
         editname.requestFocus();
         Animation shake = AnimationUtils.loadAnimation(SignIn.this, R.anim.shake);
@@ -62,9 +91,85 @@ public EditText userName;
 //        ssbuilder.setSpan(fgcspan, 0, msg.length(), 0);
         editname.setError(msg);
     }
-    public void onBackPressed(){
-        Intent i = new Intent(SignIn.this,NavigationDrawerPage.class);
+
+    public void onBackPressed() {
+        Intent i = new Intent(SignIn.this, NavigationDrawerPage.class);
         startActivity(i);
         finish();
     }
+
+    class LoginAsyncTask extends AsyncTask<String, Void, Boolean> {
+
+        String msg = "", response_status = "";
+        int status;
+        String Response = null;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+
+        }
+
+        @Override
+        protected Boolean doInBackground(String... urls) {
+
+            // Session class instance
+            //  session_manager = new SessionManager(ActionBarActivity_Subclass_Homepage.this);
+            // commonsession = new CommonIDSessionManager(ActionBarActivity_Subclass_Homepage.this);
+
+            // get user data from session
+            //   HashMap<String, String> user = commonsession.getUserDetails();
+            // commonid = user.get(CommonIDSessionManager.KEY_COMMONID);
+
+            // code to post data to server
+
+            try {
+                HttpClient httpclient = new DefaultHttpClient();
+                httpclient.getParams().setParameter(CoreProtocolPNames.USER_AGENT, "thomsomobileapp");
+                HttpPost httppost = new HttpPost(urls[0]);
+
+                List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>(1);
+
+                nameValuePairs.add(new BasicNameValuePair("username", mUsername));
+                nameValuePairs.add(new BasicNameValuePair("password", mPassword));
+                httppost.setEntity(new UrlEncodedFormEntity(nameValuePairs));
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                Response = EntityUtils.toString(entity);
+                status = response.getStatusLine().getStatusCode();
+
+                if (status == 200) {
+                    JSONObject object = new JSONObject(Response);
+                    response_status = object.getString("status");
+                    msg = object.getString("message");
+
+                    if (response_status.equalsIgnoreCase("1")) {
+                        Toast.makeText(SignIn.this, "Logged In!!!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IllegalStateException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+
+            return null;
+
+        }
+
+        @Override
+        protected void onPostExecute(Boolean result) {
+
+
+        }
+    }
+
 }
--- app/src/main/java/com/example/tarun/thomso2k16/SplashPage.java
+++ app/src/main/java/com/example/tarun/thomso2k16/SplashPage.java
@@ -7,11 +7,33 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import java.util.HashMap;
 import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import android.os.Handler;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.DefaultRetryPolicy;
+import com.android.volley.NetworkError;
+import com.android.volley.NoConnectionError;
+import com.android.volley.ParseError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.ServerError;
+import com.android.volley.TimeoutError;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SplashPage extends AppCompatActivity {
     // Splash screen timer
     private static int SPLASH_TIME_OUT = 2000;
+    public JsonObjectRequest jsonObjReq;
       public Context context;
     // Session Manager Class
     SessionManager session;
@@ -40,14 +62,18 @@ public class SplashPage extends AppCompatActivity {
                 //CLEAR IMAGE CACHE FROM PHONE SD CARD
                // FileCache aa=new FileCache(SplashPage.this);
                 //aa.clear();
-                context.deleteDatabase("THOMSO.db");
-                Events_pojo event = new Events_pojo();
-                event.setEventName("Name1");
-                event.setEventDescription("Description1");
-                event.setEventDate("Date1");
-                event.setEventTime("time1");
-                event.setEventVenue("venue1");
-                dbh.getInput(event);
+
+              //  Events_pojo event = new Events_pojo();
+               // event.setEventName("Name1");
+               // event.setEventDescription("Description1");
+               // event.setEventDate("Date1");
+               // event.setEventTime("time1");
+               // event.setEventVenue("venue1");
+              // dbh.getInput(event);
+                if(cic.isConnectingToInternet()){
+                    context.deleteDatabase("THOMSO.db");
+                    JsonRequest("http://dev-test.co.nf/events.php");
+                }
                 if (session.isLoggedIn())
                 {
 //					Random r = new Random(System.currentTimeMillis());
@@ -79,5 +105,79 @@ public class SplashPage extends AppCompatActivity {
         if(isInternetPresent){
             // call json and update local db
         }
+    }
+    private void JsonRequest(String Url)
+    {
+
+
+        System.out.println("------------Events------------------"+Url);
+
+        jsonObjReq = new JsonObjectRequest(Request.Method.GET,Url, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response)
+            {
+
+                System.out.println("------------Events Response--------"+response);
+                try {
+
+                    JSONArray jarray = response.getJSONArray("Day0");
+                    if(jarray.length()>0)
+                    {
+                        for (int i = 0; i < jarray.length(); i++)
+                        {
+                            JSONObject object = jarray.getJSONObject(i);
+
+
+                            Events_pojo event = new Events_pojo();
+                            event.setEventName(object.getString("Name"));
+                            event.setEventDescription(object.getString("Description"));
+                            event.setEventDate(object.getString("Date"));
+                            event.setEventTime(object.getString("Time"));
+                            event.setEventVenue(object.getString("Venue"));
+                            dbh.getInput(event);
+                        }
+                    }
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+
+
+                //------on post execute-----
+
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error)
+            {
+
+
+                if (error instanceof TimeoutError || error instanceof NoConnectionError) {
+                    Toast.makeText(context,"Unable to fetch data from server",Toast.LENGTH_LONG).show();
+                } else if (error instanceof AuthFailureError) {
+                    //TODO
+                    Toast.makeText(context,"AuthFailureError",Toast.LENGTH_LONG).show();
+                } else if (error instanceof ServerError) {
+                    //TODO
+                    Toast.makeText(context,"ServerError",Toast.LENGTH_LONG).show();
+                } else if (error instanceof NetworkError) {
+
+                    Toast.makeText(context,"NetworkError",Toast.LENGTH_LONG).show();
+                    //TODO
+                } else if (error instanceof ParseError) {
+                    //TODO
+                    Toast.makeText(context,"ParseError",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
+        jsonObjReq.setRetryPolicy(new DefaultRetryPolicy(30000,
+                DefaultRetryPolicy.DEFAULT_MAX_RETRIES,
+                DefaultRetryPolicy.DEFAULT_BACKOFF_MULT));
+
+        // Adding request to request queue
+        AppController.getInstance().addToRequestQueue(jsonObjReq);
     }
 }
